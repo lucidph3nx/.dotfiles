e62ac29f658897b98539bf27a9806c760b71db22
lucidph3nx <ben@tinfoilforest.nz>
2022-12-29 14:16:40 +1300

    new install changes

--- .config/zsh/utils.zsh
+++ .config/zsh/utils.zsh
@@ -10,27 +10,27 @@ source /usr/share/nvm/init-nvm.sh
 export PATH="${PATH}:${HOME}/.krew/bin"
 
 # --- asdf version manager
-. /opt/asdf-vm/asdf.sh
+# . /opt/asdf-vm/asdf.sh
 
 # --- jenv - java version manager
-eval export PATH="/home/ben/.jenv/shims:${PATH}"
-export JENV_SHELL=zsh
-export JENV_LOADED=1
-unset JAVA_HOME
-unset JDK_HOME
-jenv rehash 2>/dev/null
-jenv refresh-plugins
-jenv() {
-  type typeset &> /dev/null && typeset command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  enable-plugin|rehash|shell|shell-options)
-    eval `jenv "sh-$command" "$@"`;;
-  *)
-    command jenv "$command" "$@";;
-  esac
-}
+# eval export PATH="/home/ben/.jenv/shims:${PATH}"
+# export JENV_SHELL=zsh
+# export JENV_LOADED=1
+# unset JAVA_HOME
+# unset JDK_HOME
+# jenv rehash 2>/dev/null
+# jenv refresh-plugins
+# jenv() {
+#   type typeset &> /dev/null && typeset command
+#   command="$1"
+#   if [ "$#" -gt 0 ]; then
+#     shift
+#   fi
+# 
+#   case "$command" in
+#   enable-plugin|rehash|shell|shell-options)
+#     eval `jenv "sh-$command" "$@"`;;
+#   *)
+#     command jenv "$command" "$@";;
+#   esac
+# }
