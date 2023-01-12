46affcf10a2c0407092d18a22a438f3e1f970cb8
lucidph3nx <ben@tinfoilforest.nz>
2023-01-13 10:59:20 +1300

    added more missing nvim files

---
+++ .config/nvim/after/plugin/autopairs.lua
@@ -0,0 +1,6 @@
+require('nvim-autopairs').setup({
+  disable_filetype = {
+    "TelescopePrompt",
+    "vim"
+  },
+})
---
+++ .config/nvim/after/plugin/init.lua
@@ -0,0 +1,6 @@
+require('nvim-tree').setup({
+    filters = {
+       --this SHOWS dotfiles
+        dotfiles = false, 
+    },
+})
