537c00fcb9c8a1563928e0af974d2ad52c7aab34
lucidph3nx <ben@tinfoilforest.nz>
2023-01-26 12:20:40 +1300

    fixed bootstrap and changed keybinds

--- .config/nvim/lua/lucidph3nx/plugins.lua
+++ .config/nvim/lua/lucidph3nx/plugins.lua
@@ -1,7 +1,54 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+---@diagnostic disable-next-line: missing-parameter
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  -- snapshot = "july-24",
+  snapshot_path = fn.stdpath "config" .. "/snapshots",
+  max_jobs = 50,
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+    prompt_border = "rounded", -- Border style of prompt popups.
+  },
+}
 return require('packer').startup(function(use)
+  -- Plugin Manager
   use 'wbthomason/packer.nvim'
+  -- Theme
   use 'navarasu/onedark.nvim'
   use 'kyazdani42/nvim-web-devicons'
+  -- Utilities
   use 'kyazdani42/nvim-tree.lua' -- a sidebar file explorer
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
@@ -38,6 +85,7 @@ return require('packer').startup(function(use)
   use 'windwp/nvim-autopairs' -- automatically close brackets
   use 'akinsho/toggleterm.nvim' -- terminal
   use 'norcalli/nvim-colorizer.lua' -- show hex colours etc
+  -- AI
   --use 'github/copilot.vim'
   use {
     "zbirenbaum/copilot.lua",
@@ -48,5 +96,11 @@ return require('packer').startup(function(use)
       end, 100)
     end,
   }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
 
