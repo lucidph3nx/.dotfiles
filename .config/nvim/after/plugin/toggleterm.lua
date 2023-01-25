fa2d0daf1e508c1848018849aa0f9de138d39276
lucidph3nx <ben@tinfoilforest.nz>
2023-01-26 12:09:32 +1300

    floating term for toggleterm

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -5,4 +5,11 @@ require("toggleterm").setup {
     shell = "zsh",
     hide_numbers = true,
     close_on_exit = true,
+    direction = "float",
+    float_opts = {
+        border = "single",
+        width = 100,
+        height = 30,
+        winblend = 3,
+    },
 }
