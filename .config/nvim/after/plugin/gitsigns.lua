fbd7793a61f8c9c4f876c292157dbf390f819b17
lucidph3nx <ben@tinfoilforest.nz>
2023-03-15 09:57:23 +1300

    more shortcuts for git workflows

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -7,5 +7,15 @@ require('gitsigns').setup({
       delay = 200,
       ignore_whitespace = false,
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+      opts = { buffer = bufnr, remap = false}
+      -- keymaps
+      vim.keymap.set('n', '<leader>{', gs.prev_hunk, opts)
+      vim.keymap.set('n', '<leader>}', gs.next_hunk, opts)
+
+      vim.keymap.set('n', '<leader>td', gs.toggle_deleted, opts)
+      vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame, opts)
+    end
 })
 
--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -15,6 +15,7 @@ telescope.setup {
 
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>st', builtin.git_files, { desc = '[S]earch [T]racked files (git)' })
+vim.keymap.set('n', '<leader>sc', builtin.git_commits, { desc = '[S]earch [C]ommits (git)' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
