b3596ce828f5d9db75bc299447ca607c13331a08
lucidph3nx <ben@tinfoilforest.nz>
2023-03-15 10:31:34 +1300

    add icons back to lualine

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,6 +1,6 @@
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'onedark',
     component_separators = '|',
     section_separators = '',
