facab00a3872ab484ee0cf73ec3c3281c654d229
lucidph3nx <ben@tinfoilforest.nz>
2023-03-25 18:31:28 +1300

    formatting

--- .config/nvim/lua/lucidph3nx/plugins.lua
+++ .config/nvim/lua/lucidph3nx/plugins.lua
@@ -51,40 +51,40 @@ return require('packer').startup(function(use)
   -- Utilities
   use 'kyazdani42/nvim-tree.lua' -- a sidebar file explorer
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
-  use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
   use 'theprimeagen/harpoon'
   use 'mbbill/undotree'
-  use 'tpope/vim-fugitive' -- git integraton
-  use 'lewis6991/gitsigns.nvim' -- inline git commit history
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  use 'tpope/vim-fugitive'                  -- git integraton
+  use 'lewis6991/gitsigns.nvim'             -- inline git commit history
+  use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- comment visually selected regions with 'gc'
+  use 'numToStr/Comment.nvim'               -- comment visually selected regions with 'gc'
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
     }
   }
-  use 'windwp/nvim-autopairs' -- automatically close brackets
-  use 'akinsho/toggleterm.nvim' -- terminal
+  use 'windwp/nvim-autopairs'       -- automatically close brackets
+  use 'akinsho/toggleterm.nvim'     -- terminal
   use 'norcalli/nvim-colorizer.lua' -- show hex colours etc
   -- use 'epwalsh/obsidian.nvim'
   -- AI
@@ -100,4 +100,3 @@ return require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
-
