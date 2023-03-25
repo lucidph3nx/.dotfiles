73a7de88fd74c655b08b1daad0a3585f3f5a33d4
lucidph3nx <ben@tinfoilforest.nz>
2023-03-26 11:40:18 +1300

    improve sessionizer, fix some keymaps

--- .config/zsh/mappings.zsh
+++ .config/zsh/mappings.zsh
@@ -3,6 +3,6 @@ bindkey -s ^f "tmux-sessionizer\n"
 # Ctrl-k open k9s
 bindkey -s ^k "k9s\n"
 # Ctrl-v open nvim in current directory
-bindkey -s ^v "nvim .\n"
+bindkey -s ^v "nvim\n"
 # Ctrl-p open python
 bindkey -s ^p "python\n"
