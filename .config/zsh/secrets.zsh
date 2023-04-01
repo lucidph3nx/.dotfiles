607b92d1ddf2217b4e543fa39e52948870964b17
lucidph3nx <ben@tinfoilforest.nz>
2023-04-02 09:23:35 +1200

    encrypted secrets in git

---
+++ .config/zsh/secrets.zsh
@@ -0,0 +1,10 @@
+## load secrets from sops
+SOPS_AGE_KEY_FILE="$XDG_CONFIG_HOME/sops/age/age/keys.txt"
+SECRETS_FILE="$XDG_CONFIG_HOME/secrets/encrypted.sops"
+# decrypt secrets if age key is present
+if [[ -f $SOPS_AGE_KEY_FILE ]] && [[ -f $SECRETS_FILE ]]; then
+  eval "$(sops --decrypt $SECRETS_FILE)"
+else
+  echo "Warning: Age key not found or secrets file not present. Skipping secrets decryption."
+fi
+
