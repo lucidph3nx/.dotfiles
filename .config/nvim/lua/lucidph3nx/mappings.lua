faceaec9a03cce730181ba4636dc1804ccf665b0
lucidph3nx <ben@tinfoilforest.nz>
2023-04-15 15:12:00 +1200

    add some descriptions for my keymaps

--- .config/nvim/lua/lucidph3nx/mappings.lua
+++ .config/nvim/lua/lucidph3nx/mappings.lua
@@ -16,32 +16,29 @@ map('v', '<leader>p', '"+p', opts)
 map('v', '<leader>P', '"+P', opts)
 
 -- Helix inspired redo
-map('n', 'U', '<C-r>', opts)
+map('n', 'U', '<C-r>', make_opts('Redo', opts))
 
 
 -- when something is highlighted, move up and down with J & K
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
+map("v", "J", ":m '>+1<CR>gv=gv", make_opts('Move line down', opts))
+map("v", "K", ":m '<-2<CR>gv=gv", make_opts('Move line up', opts))
 
 -- quickfix list
-map("n", "<leader>q", "<cmd>copen<CR>", opts)
-map("n", "<C-k>", "<cmd>cnext<CR>", opts)
-map("n", "<C-j>", "<cmd>cprev<CR>", opts)
--- quickfix on current window
-map("n", "<leader>k", "<cmd>lnext<CR>", opts)
-map("n", "<leader>j", "<cmd>lprev<CR>", opts)
+map("n", "<leader>q", "<cmd>copen<CR>", make_opts('Open quickfix list', opts))
+map("n", "<C-k>", "<cmd>cnext<CR>", make_opts('Next quickfix item', opts))
+map("n", "<C-j>", "<cmd>cprev<CR>", make_opts('Previous quickfix item', opts))
 
 -- switch to new session in tmux
-map("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>", opts)
+map("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>", make_opts('Switch to new tmux session', opts))
 
 -- start a replace with current word
-map("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
+map("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], make_opts('[F]ind and [R]eplace', opts))
 -- same but with current visual selection
-map("v", "<leader>fr", [[y<Esc>:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
+map("v", "<leader>fr", [[y<Esc>:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], make_opts('[F]ind and [R]eplace', opts))
 -- make current file executable
-map("n", "<leader>x", "<cmd>!chmod +x %<CR>", opts)
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", make_opts('Make file executable', opts))
 
 -- encrypt file with sops
-map('n', '<leader>ds', ':lua sops_helper.decrypt()<CR>', opts)
-map('n', '<leader>es', ':lua sops_helper.encrypt()<CR>', opts)
+map('n', '<leader>ds', ':lua sops_helper.decrypt()<CR>', make_opts('[D]ecrypt [S]ops file', opts))
+map('n', '<leader>es', ':lua sops_helper.encrypt()<CR>', make_opts('[E]ncrypt [S]ops file', opts))
 
