51367de6a93e415d7964c51e1fa1e859e920ff51
lucidph3nx <ben@tinfoilforest.nz>
2023-04-20 12:58:16 +1200

    custom theme for insomnia

---
+++ .config/Insomnia/plugins/insomnia-plugin-one-dark-custom/one-dark-custom.js
@@ -0,0 +1,66 @@
+module.exports = {
+  name: 'one-dark-custom',
+  displayName: 'One Dark Custom',
+  theme: {
+    background: {
+      default: '#272c35',
+      success: '#98c379',
+      notice: '#61afef',
+      warning: '#d19a66',
+      danger: '#e06c75',
+      surprise: '#61afef',
+      info: '#56b6c2',
+    },
+    foreground: {
+      default: '#fff',
+      success: '#fff',
+      notice: '#fff',
+      warning: '#fff',
+      danger: '#fff',
+      surprise: '#fff',
+      info: '#fff',
+    },
+    highlight: {
+      default: 'rgba(114, 121, 133, 1)',
+      xxs: 'rgba(114, 121, 133, 0.05)',
+      xs: 'rgba(114, 121, 133, 0.1)',
+      sm: 'rgba(114, 121, 133, 0.2)',
+      md: 'rgba(114, 121, 133, 0.4)',
+      lg: 'rgba(114, 121, 133, 0.6)',
+      xl: 'rgba(114, 121, 133, 0.8)',
+    },
+    styles: {
+      appHeader: {
+        background: {
+          default: '#282c34',
+        },
+      },
+      sidebar: {
+        background: {
+          default: '#282c34',
+        },
+      },
+      dialog: {
+        background: {
+          default: '#282c34',
+        },
+      },
+      paneHeader: {
+        background: {
+          success: '#98c379',
+          notice: '#61afef',
+          warning: '#d19a66',
+          danger: '#d86a6f',
+          surprise: '#61afef',
+          info: '#51acb7',
+        },
+      },
+      transparentOverlay: {
+        background: {
+          default: 'rgba(30, 33, 40, 0.8)',
+        },
+      },
+    },
+  },
+};
+
