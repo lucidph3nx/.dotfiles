d0f7d1cfe1d1f850a51b630438bc9567acedebc9
lucidph3nx <ben@tinfoilforest.nz>
2023-04-29 16:21:27 +1200

    remove the close on no other windows for nvim-tree, stops me opening any directory...

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -38,15 +38,15 @@ end
 -- vim.api.nvim_create_autocmd("VimEnter", { callback = open_nvim_tree })
 
 -- attempt at autoclose
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
-    end
-  end
-})
-
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   nested = true,
+--   callback = function()
+--     if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+--       vim.cmd "quit"
+--     end
+--   end
+-- })
+--
 vim.keymap.set('n', '<leader>et', vim.cmd.NvimTreeToggle, { desc = '[E]xplorer [T]oggle' })
 vim.keymap.set('n', '<leader>ef', vim.cmd.NvimTreeFindFile, { desc = '[E]xplorer [F]ind File' })
 vim.keymap.set('n', '<leader>ec', vim.cmd.NvimTreeCollapse, { desc = '[E]xplorer [C]ollapse Folders' })
