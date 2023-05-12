c55122cce4374e753e06d1633a079300863dfec8
lucidph3nx <ben@tinfoilforest.nz>
2023-05-12 15:17:24 +1200

    fix icons for sway and waybar

--- .config/nvim/lua/lucidph3nx/mappings.lua
+++ .config/nvim/lua/lucidph3nx/mappings.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = ' '
 -- don't forget to install wl-clipboard
 -- Copy to clipboard
 map('v', '<leader>y', '"+y', opts)
-map('n', '<leader>Y',  '"+yg_', opts) --copy to end of file
+map('n', '<leader>Y',  '"+yG', opts) --copy to end of file
 map('n', '<leader>y',  '"+y', opts)
 map('n', '<leader>yy',  '"+yy', opts) --copy whole line
 -- Paste from clipboard
