c725ce6d297f65f3b32652bf3a11596854dcbf78
lucidph3nx <ben@tinfoilforest.nz>
2023-06-14 20:12:27 +1200

    add python binaries to path

--- .config/zsh/utils.zsh
+++ .config/zsh/utils.zsh
@@ -9,3 +9,6 @@ export PATH="${PATH}:${HOME}/.local/share/cargo/bin"
 # adds krew k8s package manager to path
 # krew (kubectl package manager)
 export PATH="${PATH}:${HOME}/.krew/bin"
+
+# add python binaries to PATH
+export PATH="${PATH}:${HOME}/.local/share/python/bin"
