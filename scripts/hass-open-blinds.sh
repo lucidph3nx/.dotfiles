a2cf0a0b3ff7bb886e73fd7c828700f2e9432851
lucidph3nx <ben@tinfoilforest.nz>
2023-08-28 08:41:01 +1200

    moving more scripts to command pallate

--- scripts/hass-open-blinds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# open office blinds
-curl -X POST \
-  -H "Authorization: Bearer ${HASS_API_KEY}" \
-  -H "Content-Type: application/json" \
-  -d '{"entity_id": "cover.office_blinds"}' \
-  https://home-assistant.tinfoilforest.nz/api/services/cover/open_cover
