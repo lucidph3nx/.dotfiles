e235138e9324c41134e73ffd6be21ab931d41549
lucidph3nx <ben@tinfoilforest.nz>
2023-09-24 09:40:36 +1300

    fix keystore time for bw in qute

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -93,10 +93,10 @@ c.bindings.commands['normal'] = {
     'ch': 'tab-only --next',
     'cl': 'tab-only --prev',
     # bitwarden bindings
-    '<Space>ll': 'spawn --userscript qute-bitwarden --totp',
-    '<Space>lu': 'spawn --userscript qute-bitwarden --username-only',
-    '<Space>lp': 'spawn --userscript qute-bitwarden --password-only',
-    '<Space>lt': 'spawn --userscript qute-bitwarden --totp-only',
+    '<Space>ll': 'spawn --userscript qute-bitwarden --auto-lock 604800 --totp',
+    '<Space>lu': 'spawn --userscript qute-bitwarden --auto-lock 604800 --username-only',
+    '<Space>lp': 'spawn --userscript qute-bitwarden --auto-lock 604800 --password-only',
+    '<Space>lt': 'spawn --userscript qute-bitwarden --auto-lock 604800 --totp-only',
     # youtube music download
     '<Space>md': 'spawn --userscript ytm-download',
     '<Space>y': 'yank selection',
