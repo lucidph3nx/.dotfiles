e7ce53451eb17c76916d12254563a3f018d4ef60
lucidph3nx <ben@tinfoilforest.nz>
2023-11-16 12:33:22 +1300

    add bw session to macos zsh

--- .config/zsh/secrets.zsh
+++ .config/zsh/secrets.zsh
@@ -4,6 +4,10 @@ SECRETS_FILE="$XDG_CONFIG_HOME/secrets/encrypted.sops"
 # decrypt secrets if age key is present
 if [[ -f $SOPS_AGE_KEY_FILE ]] && [[ -f $SECRETS_FILE ]]; then
   eval "$(sops --decrypt $SECRETS_FILE)"
+  # set bw session on mac because it isnt done at os level
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    export BW_SESSION=$(bw unlock --raw $BW_KEY)
+  fi
 else
   echo "Warning: Age key not found or secrets file not present. Skipping secrets decryption."
 fi
