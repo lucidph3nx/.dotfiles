d9d2fa601b7840ea52876d4538dd1de69bb47b21
lucidph3nx <ben@tinfoilforest.nz>
2024-03-19 13:46:26 +1300

    remove old code block

--- .config/zsh/secrets.zsh
+++ .config/zsh/secrets.zsh
@@ -4,10 +4,6 @@ SECRETS_FILE="$XDG_CONFIG_HOME/secrets/encrypted.sops"
 # decrypt secrets if age key is present
 if [[ -f $SOPS_AGE_KEY_FILE ]] && [[ -f $SECRETS_FILE ]]; then
   eval "$(sops --decrypt $SECRETS_FILE)"
-  # set bw session on mac because it isnt done at os level
-#   if [[ "$OSTYPE" == "darwin"* ]]; then
-#     export BW_SESSION=$(bw unlock --raw $BW_KEY 2>/dev/null)
-#   fi
 else
   echo "Warning: Age key not found or secrets file not present. Skipping secrets decryption."
 fi
